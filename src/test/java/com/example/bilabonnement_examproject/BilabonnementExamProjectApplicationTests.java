24a5aa7a0022665dfc323f47ac0eab64c16f591e
LauraStorm <lbs2rm@hotmail.com>
2022-05-05 10:09:37 +0200

    start

---
+++ src/test/java/com/example/bilabonnement_examproject/BilabonnementExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnement_examproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnementExamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
