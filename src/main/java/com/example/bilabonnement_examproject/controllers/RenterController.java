fcea39b530b4dcbd896f722397647424eddd537d
rasmuswoldike <rswoldike@gmail.com>
2022-05-10 15:55:21 +0200

    repo

--- src/main/java/com/example/bilabonnement_examproject/controllers/RenterController.java
+++ src/main/java/com/example/bilabonnement_examproject/controllers/RenterController.java
@@ -1,12 +1,20 @@
 package com.example.bilabonnement_examproject.controllers;
 
+import com.example.bilabonnement_examproject.models.RenterModel;
+import com.example.bilabonnement_examproject.repositories.RenterRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RenterController {
+
     @GetMapping("/create-renter-information")
     public String getCreateRenterPage(){
+
+        RenterModel renterModel = new RenterModel("tim", "tim", "kea", 6089, "kbh", "jsjds@jskdk.ksdk", 12345678, 982939000, 92892003, 82839923);
+        RenterRepo renterRepo = new RenterRepo();
+        renterRepo.createEntity(renterModel);
+
         return "create-renter-information";
     }
 }
--- src/main/java/com/example/bilabonnement_examproject/repositories/RenterRepo.java
+++ src/main/java/com/example/bilabonnement_examproject/repositories/RenterRepo.java
@@ -1,7 +1,11 @@
 package com.example.bilabonnement_examproject.repositories;
 
 import com.example.bilabonnement_examproject.models.RenterModel;
+import com.example.bilabonnement_examproject.utility.DatabaseConnectionManager;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class RenterRepo implements CRUDInterface<RenterModel>{
@@ -17,6 +21,29 @@ public class RenterRepo implements CRUDInterface<RenterModel>{
 
     @Override
     public boolean createEntity(RenterModel entity) {
+        Connection conn = DatabaseConnectionManager.getConnection();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO renters(first_name, last_name, adress, postcode, city, email, " +
+                    "tlf_number, cpr_number, reg_number, account_number) VALUES (?,?,?,?,?,?,?,?,?,?)");
+            stmt.setString(1, entity.getFirstName());
+            stmt.setString(2,entity.getLastName());
+            stmt.setString(3, entity.getAddress());
+            stmt.setInt(4, entity.getPostcode());
+            stmt.setString(5, entity.getCity());
+            stmt.setString(6, entity.getEmail());
+            stmt.setInt(7,entity.getTlf());
+            stmt.setInt(8, entity.getCpr());
+            stmt.setInt(9, entity.getRegNumber());
+            stmt.setInt(10, entity.getAccountNumber());
+
+            stmt.executeUpdate();
+            System.out.println("renter created");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("renter not created");
+        }
+
         return false;
     }
 
